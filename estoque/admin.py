498eae4e709149a0c2f84e7aa398bc1089d205f6
CamilaBelo15 <camilabelo2000@gmail.com>
2025-06-12 21:48:54 -0400

    models

--- estoque/admin.py
+++ estoque/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Produto,Entrada,Saida
+
+admin.site.register(Produto)
+admin.site.register(Entrada)
+admin.site.register(Saida)
--- estoque/models.py
+++ estoque/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField(blank=True, null=True)
+    valor = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    marca = models.CharField(max_length=100, blank=True, null=True)
+    tamanho = models.CharField(max_length=50, blank=True, null=True)
+
+
+class Entrada(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE, related_name='entradas')
+    quantidade = models.IntegerField()
+    data_entrada = models.DateTimeField
+
+
+class Saida(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE, related_name='saidas')
+    quantidade = models.IntegerField()
+    data_saida = models.DateTimeField
+
+
+
